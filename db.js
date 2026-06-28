a548fa1357eb0667e2b29ad3c506954c0221f79d
Nick <151847767+Jemohke@users.noreply.github.com>
2026-06-29 01:17:40 +0300

    fix: use SESSION_DB_URL instead of DATABASE_URL

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import crypto from 'crypto';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.SESSION_DB_URL,
   ssl: { rejectUnauthorized: false },
   max: 3,
   idleTimeoutMillis: 30000,
